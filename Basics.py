e60e2e7b779f5ab1e1eef31a6ac0f449ed93315a
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-02-11 07:45:38 +0530

    python_Basics_01

---
+++ Basics.py
@@ -0,0 +1,12 @@
+# User_Name = input("Enter your Name: ")
+
+# print("Hello,", User_Name)
+
+
+num_1 = int(input("Enter Number 1: "))
+
+num_2 = int(input("Enter Number 2: "))
+
+Total = (num_1 + num_2)
+
+print("Total is : ",Total)
