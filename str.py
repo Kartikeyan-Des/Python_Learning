9b2ba6dd1609fa6fab3259fd90b26cd5ba8ff92f
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-02-24 08:49:20 +0530

    day 11 strings
    learnt string to use it with loops and if else also the other built in functions too

--- str.py
+++ str.py
@@ -1,1 +1,19 @@
-input("Enter Palindrom Word: ")
+word = input("Enter Palindrom Word: ").lower()
+
+reversed_word = word[::-1]
+if reversed_word == word:
+  print(f"{reversed_word} is a Palindrome")
+elif reversed_word != word:
+  print(f"{reversed_word} is not a palindrome")
+
+sentence = input("Enter a sentence: ").lower()
+
+vowels = "aeiou"
+
+count = 0 
+
+for char in sentence:
+  if char in vowels:
+    count += 1
+
+print("Number of Vowels: ",count)
