662fd17bf5505c0131808f2c09a38fded84f5f23
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-03-18 09:03:28 +0530

    Aoi implemented and worked , now polish is pending

--- Weather App/main.py
+++ Weather App/main.py
@@ -1,8 +1,23 @@
+from dotenv import load_dotenv
+import os
 import requests
 
-url = "YOUR_API_KEY_HERE"
+load_dotenv()
 
-response = requests.get(url)
+API_KEY = os.getenv("API_KEY")
+
+if not API_KEY:
+    raise ValueError("API_KEY not found")
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+params = {
+    "lat": 13.0827,
+    "lon": 80.2707,
+    "appid": API_KEY
+}
+
+response = requests.get(url, params=params)
 
 if response.status_code == 200:
     data = response.json()
