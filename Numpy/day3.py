5d187992fe99d73ffb276978b647b0b1b4ad2c77
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-04-08 08:05:39 +0530

    Update day3.py

--- Numpy/day3.py
+++ Numpy/day3.py
@@ -1,41 +1,80 @@
 import numpy as np
 
-# 1
-arr1 = np.array([10,20,30,40,50])
+# # 1
+# arr1 = np.array([10,20,30,40,50])
 
-div = arr1 / 2
+# div = arr1 / 2
 
-print(div)
+# print(div)
 
-# 2
+# # 2
 
-mat = np.array([[1, 2, 3], 
-                [4, 5, 6], 
-                [7, 8, 9]])
+# mat = np.array([[1, 2, 3], 
+#                 [4, 5, 6], 
+#                 [7, 8, 9]])
 
-print(mat [1:, :2])
+# print(mat [1:, :2])
 
-# 3
+# # 3
 
-mat = np.array([[1, 2, 3], 
-                 [4, 5, 6], 
-                 [7, 8, 9]])
+# mat = np.array([[1, 2, 3], 
+#                  [4, 5, 6], 
+#                  [7, 8, 9]])
 
-col_sum = np.sum(mat, axis= 0)
+# col_sum = np.sum(mat, axis= 0)
 
-print(col_sum)
+# print(col_sum)
 
-# 4
-mat = np.zeros((4,4))
+# # 4
+# mat = np.zeros((4,4))
 
-mat = mat+10
+# mat = mat+10
 
-print(mat)
+# print(mat)
 
-# 5
+# # 5
 
-vals = np.array([1, 8, 2, 10, 3, 15])
+# vals = np.array([1, 8, 2, 10, 3, 15])
 
-gr = vals[vals > 5]
+# gr = vals[vals > 5]
 
-print(gr)
+# print(gr)
+
+#6
+
+sp = np.arange(12)
+
+re = sp.reshape(3,4)
+
+print(re)
+
+#7
+
+data = np.array([10, -5, 20, -1, 30, -8])
+
+data[data < 0] = 0
+
+loc = np.where(data == 30)
+
+print(data)
+#8
+print(loc)
+
+
+#9
+
+spl = np.random.randint(1, 101, size=(4, 3))
+
+max = np.max(spl, axis = 0)
+
+print(max)
+
+#10
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+# add = A@B
+add = np.dot(A, B)
+
+print(add)
