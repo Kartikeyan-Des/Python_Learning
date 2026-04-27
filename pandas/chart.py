9baa0e61660038b0dee5eb29a3b85decc3293095
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-04-27 08:19:14 +0530

    matplotlib

---
+++ pandas/chart.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.thehindu.com/news/national/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+headlines = soup.find_all("h3", class_="title")
+
+headline_list = []
+
+for headline in headlines:
+    text = headline.get_text(strip=True)
+    if text:
+        headline_list.append({"Headline" : text})
+
+df = pd.DataFrame(headline_list)
+df["Headline_length"] = df['Headline'].str.len()
+print(df.sort_values("Headline_length", ascending=False))
+
+
+
+print(f"\nTotal headlines scraped: {len(df)}")
+
+
+plt.plot(df.index, df["Headline_length"])
+plt.xlabel("Headline Index")
+plt.ylabel("Character Count")  
+plt.title("Hindu Headlines Length")
+plt.show()
+
