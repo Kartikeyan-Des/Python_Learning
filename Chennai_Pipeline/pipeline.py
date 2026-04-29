d400d11aa90fc9282adc809b278123c79dfc6641
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-04-29 08:44:47 +0530

    need gold and other price table to complete it

--- Chennai_Pipeline/pipeline.py
+++ Chennai_Pipeline/pipeline.py
@@ -1,4 +1,17 @@
 import sqlite3
+import requests
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from datetime import datetime
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+city = "Chennai"
+time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+weather_data = []
+
+if not API_KEY:
+    raise ValueError("API_KEY not found")
 
 def init_db():
     conn = sqlite3.connect("chennai_data.db")
@@ -16,3 +29,21 @@ def init_db():
     ''')
     conn.commit()
     return conn
+conn = init_db()
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+response = requests.get(url)
+data = response.json()
+
+weather_data.append({
+    "city":city,
+    "Temperature":data["main"]["temp"],
+    "Feels_Like": data["main"]["feels_like"],
+    "Humidity": data["main"]["humidity"],
+    "Weather": data["weather"][0]["description"],
+    "Timestamp": time
+})
+df = pd.DataFrame(weather_data)
+
+df.to_sql("tblWeather", conn, if_exists="append", index=False)
+print(df)
