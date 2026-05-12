950161b69bbe0f28198bb75900c9c840957c68cb
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-05-12 09:00:16 +0530

    linear regression and its factors

--- linear_regression.py
+++ linear_regression.py
@@ -2,12 +2,17 @@ import numpy as np
 
 x = np.array([1,2,3,4,5,6])
 
+x_train = x[:4]
+x_test = x[4:]
+
 y = np.array([441349, 441658, 445405, 441308, 441541, 445390])
+y_train = y[:4]
+y_test = y[4:]
 
 def  predict(X,theta0, theta1):
     return theta0 + theta1 * X
 
-result = predict(7, 373859, 16179)
+result = predict(7, 353076, 30445)
 print(result)
 
 def cost(X,Y,theta0,theta1):
@@ -15,11 +20,11 @@ def cost(X,Y,theta0,theta1):
     error = Y - predictions
     return np.mean(error**2)
 
-# print(cost(x, y, 0, 0))
+# print(cost(x, y, 353076, 0))
 
 
 
-def gradient_decent(X,Y,theta0,theta1,alpha,iterations):
+def gradient_descent(X,Y,theta0,theta1,alpha,iterations):
     n = len(Y)
     for i in range (iterations):
         predictions = predict(X, theta0, theta1)
@@ -28,5 +33,17 @@ def gradient_decent(X,Y,theta0,theta1,alpha,iterations):
     return theta0, theta1
 
 
-# t0, t1 = gradient_decent(x, y, 0, 0, 0.01, 1000)
-# print(t0, t1)
+t0, t1 = gradient_descent(x_train, y_train, 0, 0, 0.01, 1000)
+print(t0, t1)
+
+predictions = predict(x_test, t0, t1)
+print("Predicted:", predictions)
+print("Actual:", y_test)
+
+MAE = np.mean(np.abs(y_test - predictions))
+RMSE = np.sqrt(np.mean(y_test - predictions)**2)
+R = 1 - (np.sum((y_test - predictions)**2) / np.sum((y_test - np.mean(y_test))**2))
+
+print("MAE: ",MAE)
+print("RMSE: ",RMSE)
+print("R²: ",R)
