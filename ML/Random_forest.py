173ee59d5ddd258400a52d33213c66298ebbfc15
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-05-19 08:39:13 +0530

    sklearn-cross validation

--- ML/Random_forest.py
+++ ML/Random_forest.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 from sklearn.datasets import load_iris
 
 data = load_iris()
@@ -10,5 +11,14 @@ Y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-print(X_train.shape)
-print(X_test.shape)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+print("Accuracy: ", accuracy_score(y_test, predictions))
+
+scores = cross_val_score(model, X, Y, cv=5)
+
+print("CV Scores: ", scores)
+print("Average: ",scores.mean())
