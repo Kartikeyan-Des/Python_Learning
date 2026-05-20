05f0f902ea1839131d09cba6710d75fbb474cfdd
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-05-20 09:08:45 +0530

    Feature Engineering

--- ML/Random_forest.py
+++ ML/Random_forest.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -7,9 +8,11 @@ from sklearn.datasets import load_iris
 data = load_iris()
 X = data.data
 Y = data.target
+petal_ratio = X[:,2] / X[:, 3]
+X_new = np.column_stack([X,petal_ratio])
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_new, Y, test_size=0.2, random_state=42)
 
 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -18,7 +21,7 @@ model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 print("Accuracy: ", accuracy_score(y_test, predictions))
 
-scores = cross_val_score(model, X, Y, cv=5)
+scores = cross_val_score(model, X_new, Y, cv=5)
 
 print("CV Scores: ", scores)
 print("Average: ",scores.mean())
